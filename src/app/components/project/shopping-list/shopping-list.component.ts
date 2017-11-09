3cca973eb9031dbacaf0ada1041402ea66a54103
dvinubius <dvinubiu.narmon@gmail.com>
2017-11-09 11:23:42 +0200

    shopping list scaffold and basic styling. Mock data from property of shopping list component. Added model for Ingredient.

--- src/app/components/project/shopping-list/shopping-list.component.ts
+++ src/app/components/project/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('honey jar', 2),
+    new Ingredient('bread loaf', 3),
+    new Ingredient('coconut flakes pack', 4),
+    new Ingredient('dates pack', 1)
+  ];
 
   constructor() { }
 
