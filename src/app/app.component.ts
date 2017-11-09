8022d69b292eb99d7a3f57f78b2f7126035f2bcd
dvinubius <dvinubiu.narmon@gmail.com>
2017-11-09 08:43:04 +0200

    Initial setup for any assignment or project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  app_name_in_title = `Maximilian's Food App`;
+  student = 'Daniel';
 }
