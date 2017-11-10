48c71dbc107fc17a044137463a95eeb8d4d70b99
dvinubius <dvinubiu.narmon@gmail.com>
2017-11-10 21:25:30 +0200

    added navigation by event binding and ngIfs

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-project',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
+  mainRoute = true;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onNavigate(mainRoute: boolean) {
+    this.mainRoute = mainRoute;
+  }
 }
